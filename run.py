5d198a0fdb6ff6a33735008e2500f6f9bde29290
Carrie8348 <maruko_carrie@hotmail.com>
2022-01-10 09:49:16 +0000

    Add validation to check for exactly 3 values in provided data

--- run.py
+++ run.py
@@ -18,14 +18,33 @@ def get_user_info():
     Get user basic info for calculate BMR
     """
     print("Hello, this is User BMR calculating center...\n")
-    print("Please enter your name, gender, weight(kg), height(cm) and age here...\n")
-    print("Example: Jim Carter, male, 59, 165, 30\n")
+    print("Please enter your weight(kg), height(cm) and age here...\n")
+    print("Example: 59, 165, 30\n")
 
     data_str = input("Enter your data here:")
 
     user_data = data_str.split(",")
     print(f"The data provide is {data_str}")
     return user_data
+
+def validate_user_data(values):
+    """
+    Inside the try, convert strings into values, 
+    raise ValueError if strings cannot be converted into int,
+    or if there aren't exactly 3 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 3:
+            raise ValueError(
+                f"Exactly 3 values required, you provide {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data:{e}, please try again. \n")
+        return False
+    
+    return True
+
 
 def update_user_dataworksheet(data):
     """
@@ -37,5 +56,10 @@ def update_user_dataworksheet(data):
     user_worksheet.append_row(user_data)
     print("User worksheet updated successfully.\n")
 
+def calculate_bmr(user_data):
+    print('')
+
 user_data = get_user_info()
+user_data = [int(num) for num in user_data]
 update_user_dataworksheet(user_data)
+calculate_bmr(user_data)
